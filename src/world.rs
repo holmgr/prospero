59e7d4c00ba03744a9dda4e1a832da27ab57c0d8
holmgr <viktor.holmgren@gmail.com>
2019-02-28 22:48:36 +0100

    Remove anymap to be able to implement serialization

--- src/world.rs
+++ src/world.rs
@@ -1,4 +1,11 @@
-use anymap::AnyMap;
+use serde::{Deserialize, Serialize};
 
 /// Holds the world state, i.e all entities.
-pub struct World(AnyMap);
+#[derive(Serialize, Deserialize, Debug)]
+pub struct World {}
+
+impl World {
+    pub fn new() -> Self {
+        World {}
+    }
+}
