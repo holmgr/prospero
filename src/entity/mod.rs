3797b04bb56e7059165640341762135de7b03370
holmgr <viktor.holmgren@gmail.com>
2019-03-14 20:34:28 +0100

    Implement IntoIterator trait for array, ref array and ref mut array

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -50,3 +50,62 @@ where
         self.0.get_mut(index.into())
     }
 }
+
+impl<T: Entity> IntoIterator for EntityArray<T> {
+    type Item = T;
+    type IntoIter = ::std::vec::IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl<'a, T: Entity> IntoIterator for &'a EntityArray<T> {
+    type Item = &'a T;
+    type IntoIter = ::std::slice::Iter<'a, T>;
+
+    fn into_iter(self) -> ::std::slice::Iter<'a, T> {
+        self.0.iter()
+    }
+}
+
+impl<'a, T: Entity> IntoIterator for &'a mut EntityArray<T> {
+    type Item = &'a mut T;
+    type IntoIter = ::std::slice::IterMut<'a, T>;
+
+    fn into_iter(self) -> ::std::slice::IterMut<'a, T> {
+        self.0.iter_mut()
+    }
+}
+
+mod tests {
+    use super::*;
+    use crate::entity::astronomical::System;
+    use crate::point::Point;
+
+    #[test]
+    fn test_entityarray() {
+        let data = vec![
+            System::builder()
+                .location(Point::origin())
+                .name("Sol")
+                .build(),
+            System::builder()
+                .location(Point::origin())
+                .name("Alpha Centauri")
+                .build(),
+        ];
+
+        // Test get and insert.
+        let mut array = EntityArray::new();
+        for item in &data {
+            let idx = array.insert(item.clone());
+            assert_eq!(array.get(idx).unwrap(), item);
+        }
+
+        // Test iterator.
+        for (index, entity) in array.into_iter().enumerate() {
+            assert_eq!(entity, data[index]);
+        }
+    }
+}
